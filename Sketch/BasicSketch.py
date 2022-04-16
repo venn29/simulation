d4f4c8fe55c820c0b49d0e4b697bd081ba999f04
Venn Chan <865724378@qq.com>
2022-04-16 13:51:38 +0800

    temp

--- Sketch/BasicSketch.py
+++ Sketch/BasicSketch.py
@@ -1,11 +1,12 @@
 # sketch基类
 import Utility.Hash
 class _Basic_Sketch:
-    def __init__(self,d,w):
+    def __init__(self,d,w,active):
         # 哈希表长宽
+        self.active = active
         self.d = d
         self.w = w
-        # 哈希表
+        # 哈希表    二维数组
         self.sketch_table=[]
         # 工具类
         self.hash = Utility.Hash._Hash()
@@ -13,5 +14,12 @@ class _Basic_Sketch:
         self.hash_function=[]
     # 生成对应的哈希表并初始化，根据d和w初始化hash_table，为每行固定一种hash方法
     def Generate_Hash_Table(self):
-        pass
+        self.hash_function.append(self.hash.md5)
+        self.hash_function.append(self.hash.sha256)
+        for i in range (0,self.d):
+            self.sketch_table.append( [0 for x in range(0, self.w)])
+    
+    def Receive_packet(packet):
+        
+
 
--- Sketch/Switch.py
+++ Sketch/Switch.py
@@ -1,32 +1,39 @@
+from hashlib import new
 import Sketch.BasicSketch
 import Common.Packet
 class _Switch:
-    def __init__(self):
+    def __init__(self,switchid):
         # 用来存放sketch
-        self.sketch = []
+        self.Initiate_Active_Sketch(self,d,w,True)
+        self.Initiate_Idle_Sketch(self,d,w,False)
         # 后续用来存放deviation的packet
         self.hash_table = []
-        # 放一个三元组[path_ID,[α，β]，sketch[n]],表示某个路径上的范围,对应的sketch
-        self.scope = []
-        self.switch_ID = 0
+        # 放一个map[path_ID：[α，β]],表示本交换机在某个路径上的范围      对应的sketch(只有一个sketch
+        self.scope = {}
+        self.switch_ID = switchid
 
 
     # 返回对应sketch上的sketch_table内容，列表返回，比如d=2，w=3返回[[1,2,3],[1,2,3]]
 
     def Query(self,path_ID):
-
-        pass
+        return self.active_sketch.sketch_table
     # 处理正常收到的包,packet为Common.Packet._Packet
     # 根据path_ID找到对应的packet
+    # 判断是否正确，如果正确，交给basicsketch，否则交给hashsketch
     def Process_Packet(self,path_ID,packet):
-        pass
-    # 处理路径错误的包，暂时不需要
-    def Process_Deviation(self):
-        pass
+        if(self.Receive(packet)):
+            self.active_sketch.Receive_packet(packet,self.scope[path_ID])
+        else:
+            pass
     # 接收包，判断是否路径正确
-    def Receive(self):
-        pass
+    def Receive(self,packet):
+        if self.switch_ID in packet.flow.path:
+            return True
+        else:
+            return False
 
     # 生成一个新的Sketch,d,w为参数
-    def Initiate_Sketch(self,d,w):
-        pass
+    def Initiate_Active_Sketch(self,d,w,True):
+        self.active_sketch = new Sketch.BasicSketch._Basic_Sketch(d,w,True)
+    def Initiate_Idle_Sketch(self,d,w,False):
+        self.active_sketch = new Sketch.BasicSketch._Basic_Sketch(d,w,True)
